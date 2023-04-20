b5eff0d49d08bf97337a146ebe6b755f6c015ed0
grenatson <eremikonst@gmail.com>
2023-04-21 02:33:45 +0300

    it works somehow

--- nekostya/Particle.h
+++ nekostya/Particle.h
@@ -1,18 +1,25 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
+#include <iostream>
 #include "Utils.h"
 
 class Particle {
 private:
-    Vec_1d<double> velocity = Vec_1d<double>(3);
-    Vec_1d<double> position = Vec_1d<double>(3);
+    Vec_1d<double> position = Vec_1d<double>(2);
+    Vec_1d<double> velocity = Vec_1d<double>(2);
+    //Vec_1d<double> acceleration = Vec_1d<double>(2);
     double m;
     double rho;
     double p;
 
 public:
-    Particle(Vec_1d<double> position, Vec_1d<double> velocity, double m);
+    Particle(Vec_1d<double> position,
+             Vec_1d<double> velocity, 
+             //Vec_1d<double> acceleration,
+             double m
+    );
+    Particle();
 
     double distance_to(Particle& other) const;
     double W(Particle& other, double h);
@@ -20,12 +27,19 @@ public:
 
     Vec_1d<double> get_position() const;
     Vec_1d<double> get_velocity() const;
-    double get_m() const;
+    //Vec_1d<double> get_acceleration() const;
+    double get_m();
     double get_density() const;
     double get_pressure() const;
 
-    void change_velocity(Vec_1d<double>& acceleration, double dt);
-    void change_position(Vec_1d<double> velocity, double dt);
+    /*
+    void set_acceleration(Vec_1d<double> new_acceleration);
+    void inc_acceleration(Vec_1d<double> delta_acceleration);
+    */
+    void change_velocity(Vec_1d<double> acceleration, double dt);
+    //void change_velocity(Vec_1d<double> acceleration, double dt);
+    void change_position(double dt);
+    //void inc_acceleration(double delta_a_0, double delta_a_1);
     void change_density(double new_density);
     void change_pressure(double new_pressure);
 };
--- nekostya/kotyk.cpp
+++ nekostya/kotyk.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-#include <random>
 #include <unistd.h>
 #include <chrono>
 
@@ -11,68 +10,21 @@
 #include "Timer.h"
 #include "Particle.h"
 #include "Processing.h"
+#include "Visualisation.h"
 
 int main() {
-    Processing model(50);
-    //Processing::Processing(int N_part, double h, double d, double k, double n, double nu, double lambda, double dt, double maxt) : 
+    Processing model(100, 0.1, 20, 0.1, 1, 1, 4, 0.04, 12);
+    //Processing::Processing(int N_part, double h, double d, double k, double n, double nu, double lambda, double dt, double maxt)
 
     Vec_2d<Particle> result;
-    Vec_1d<Particle> particles;
-    Vec_2d<double> a(model.get_N(), Vec_1d<double>(2));
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::normal_distribution<> NormRand(0, 0.1);
-
     {
         Timer<std::chrono::seconds> t;
-
-        for (int i = 0; i < model.get_N(); i++) {
-            particles.push_back(Particle(Vec_1d<double>{NormRand(gen),
-                                                        NormRand(gen), 0},
-                                         Vec_1d<double>{NormRand(gen),
-                                                        NormRand(gen), 0},
-                                         0.03)
-            );
-        }
-
-        result = model.calc(particles);
+        result = model.calc();
     }
 
-    std::vector<sf::CircleShape> sprites(model.get_N());
-    std::size_t step = 0;
-    sf::RenderWindow window(sf::VideoMode (400, 400), " SFML works!");
-    double want_fps = 5;
-    sf::Clock loop_timer;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        for (sf::CircleShape &sprite : sprites) {
-            sprite.setRadius(1);
-            for(std::size_t j = 0; j < result[step].size(); j++) {
-                sprite.setPosition(result[step][j].get_position()[0] * 100 + 200,
-                                   result[step][j].get_position()[1] * 60 + 200);
-                window.draw(sprite);
-            }
-        }
-
-        window.display();
-
-        if (step + 1 < result.size()) step += 1;
-        else step = 0;
-
-        sf::Int32 frame_duration = loop_timer.getElapsedTime().asMilliseconds();
-        sf::Int32 time_to_sleep = int(1000.f/want_fps) - frame_duration;
-        if (time_to_sleep > 0) {
-            sf::sleep(sf::milliseconds(time_to_sleep));
-        }
-        loop_timer.restart();
-    }
-
+    Visualisation visualisation;
+    Vec_2d<double> density = model.get_density_for_all();
+    visualisation.loop(result, density, 0.1);
+    
     return 0;
 }
