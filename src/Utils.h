adb3fbe6422ed1091d18d45b5b18e37d7f0edddc
Shulliikkk <68589028+Shulliikkk@users.noreply.github.com>
2023-04-21 15:15:42 +0300

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -10,10 +10,6 @@ template<typename T>
 using Vec_2d = std::vector<std::vector<T>>;
 template<typename T>
 using Vec_3d = std::vector<std::vector<std::vector<T>>>;
-template <typename T>
-std::ostream& operator << (std::ostream &os, sf::Vector2<T> vec) {
-    return os << "(" << vec.x << ";" << vec.y << ")";
-}
 
 const double PI = std::acos(-1.0);
 
